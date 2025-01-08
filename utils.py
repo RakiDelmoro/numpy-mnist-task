ae6d40bf5d3c628875542a254f816413541b4fd2
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-08 08:20:09 +0800

    Remove unused function

--- utils.py
+++ utils.py
@@ -9,19 +9,3 @@ def dataloader(image_arrays, label_arrays, batch_size: int, shuffle: bool):
         end = start + batch_size
         yield image_arrays[indices[start:end]], label_arrays[indices[start:end]]
 
-def train_model(train_dataloader, model_train_runner, optimizer, criterion):
-    for images, labels in train_dataloader:
-        outputs = model_train_runner(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-def test_model(test_dataloader, model_test_runner):
-    for images, labels in test_dataloader:
-        pass
-
-def model_runner(train_loader, test_loader, epochs):
-    for epoch in range(epochs):
-        # TODO: Train model
-        # TODO: Test model performance
-        pass
