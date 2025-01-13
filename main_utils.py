03dd3a352852cf57043cd2f3e98ed19e1ab151ee
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-13 21:00:05 +0800

    Rename: main to main_utils

--- main_utils.py
+++ main_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from model.mlp import model
+from modelv2.mlp import mlp
 from model.utils import parameters_init
 
 def dataloader(image_arrays, label_arrays, batch_size: int, shuffle: bool):
@@ -12,15 +12,13 @@ def dataloader(image_arrays, label_arrays, batch_size: int, shuffle: bool):
         yield image_arrays[indices[start:end]], label_arrays[indices[start:end]]
 
 def model_runner(train_images, train_labels, test_images, test_labels, model_size, batch_size, epochs, learning_rate):
-    # Model params
-    parameters = [parameters_init(model_size[i], model_size[i+1]) for i in range(len(model_size)-1)]
-    training_phase, testing_phase = model()
+    training_phase, testing_phase = mlp(size=(784, 10))
 
     for epoch in range(1, epochs+1):
         # Create a dataloaders
         trainining_loader = dataloader(train_images, train_labels, batch_size, shuffle=True)
         test_loader = dataloader(test_images, test_labels, batch_size, shuffle=False)
         # Model phases
-        train_avg_loss = training_phase(trainining_loader, parameters, learning_rate)
-        accuracy = testing_phase(test_loader, parameters)
-        print(f"EPOCH: {epoch} Train_loss: {train_avg_loss} Accuracy: {accuracy}")
+        average_loss = training_phase(trainining_loader)
+        accuracy = testing_phase(test_loader)
+        print(f"EPOCH: {epoch} Train_loss: {average_loss} Accuracy: {accuracy}")
