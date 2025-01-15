5740a75e1134801d49a16f3a5e6862de47d699e5
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-15 21:05:11 +0800

    add model parameters to run

--- main_utils.py
+++ main_utils.py
@@ -1,6 +1,4 @@
 import numpy as np
-from modelv3.mlp import mlp
-from model.utils import parameters_init
 
 def dataloader(image_arrays, label_arrays, batch_size: int, shuffle: bool):
     num_samples = image_arrays.shape[0]
@@ -11,14 +9,14 @@ def dataloader(image_arrays, label_arrays, batch_size: int, shuffle: bool):
         end = start + batch_size
         yield image_arrays[indices[start:end]], label_arrays[indices[start:end]]
 
-def model_runner(train_images, train_labels, test_images, test_labels, model_size, batch_size, epochs, learning_rate):
-    training_phase, testing_phase = mlp(size=model_size)
+def model_runner(model, train_images, train_labels, test_images, test_labels, model_size, batch_size, epochs, learning_rate):
+    training_phase, testing_phase = model
 
     for epoch in range(1, epochs+1):
         # Create a dataloaders
         trainining_loader = dataloader(train_images, train_labels, batch_size, shuffle=True)
         test_loader = dataloader(test_images, test_labels, batch_size, shuffle=False)
         # Model phases
-        average_loss = training_phase(trainining_loader)
+        average_loss = training_phase(trainining_loader, learning_rate)
         accuracy = testing_phase(test_loader)
         print(f"EPOCH: {epoch} Train_loss: {average_loss} Accuracy: {accuracy}")
