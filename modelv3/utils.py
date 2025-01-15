6411644e74b2b1eebea49c58b5ebc2b60eed656c
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-15 21:06:34 +0800

    Direct feedback error model utils

--- modelv3/utils.py
+++ modelv3/utils.py
@@ -1,7 +1,12 @@
 import math
 import torch
 import numpy as np
+from torch.nn import init
 from torch.nn.init import kaiming_uniform_
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=-1, keepdims=True)
 
 def relu(input_data, return_derivative=False):
     if return_derivative: return np.where(input_data > 0, 1, 0)
@@ -15,9 +20,13 @@ def one_hot_encoded(x_train, y_train):
 def initialize_params(model_size):
     parameters = []
     for i in range(len(model_size)-1):
-        gen_matrix = np.empty(shape=(model_size[i], model_size[i+1]))
-        weights = np.array(kaiming_uniform_(torch.tensor(gen_matrix), a=math.sqrt(5)))
-        parameters.append(weights)
+        gen_w_matrix = torch.empty(size=(model_size[i], model_size[i+1]))
+        gen_b_matrix = torch.empty(size=(model_size[i+1],))
+        weights = kaiming_uniform_(gen_w_matrix, a=math.sqrt(5))
+        fan_in, _ = init._calculate_fan_in_and_fan_out(weights)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        bias = init.uniform_(gen_b_matrix, -bound, bound)
+        parameters.append([np.array(weights), np.array(bias)])
     return parameters
 
 def initialize_random_params(model_size):
