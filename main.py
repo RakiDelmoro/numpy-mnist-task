f653a54eff056503a00ebfeda8f3fe2b07e3cb4a
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-20 21:10:17 +0800

    clean

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import gzip
 import pickle
+from model.mlp import model
+from modelv3.mlp import model
 from main_utils import model_runner
 
 def runner():
     EPOCHS = 100
-    BATCH_SIZE = 2048
+    BATCH_SIZE = 1
     LEARNING_RATE = 0.001
     IMAGE_HEIGHT = 28
     IMAGE_WIDTH = 28
-    MODEL_ARCHITECTURE = [IMAGE_HEIGHT * IMAGE_WIDTH, 1000, 1000, 1000, 1000, 1000, 10]
-
+    MODEL_ARCHITECTURE = [IMAGE_HEIGHT * IMAGE_WIDTH, 1420, 1420, 10]
+    
+    MODEL = model(MODEL_ARCHITECTURE)
+    # MODEL = model(MODEL_ARCHITECTURE)
+    
     # Load MNIST-Data into memory
     with gzip.open('./mnist-data/mnist.pkl.gz', 'rb') as f: ((train_images, train_labels), (test_images, test_labels), _) = pickle.load(f, encoding='latin1')
     # Validate data shapes 
@@ -17,6 +22,6 @@ def runner():
     assert test_images.shape[0] == test_labels.shape[0]
     assert train_images.shape[1] == test_images.shape[1] == IMAGE_HEIGHT * IMAGE_WIDTH
 
-    model_runner(train_images, train_labels, test_images, test_labels, MODEL_ARCHITECTURE, BATCH_SIZE, EPOCHS, LEARNING_RATE)
+    model_runner(MODEL, train_images, train_labels, test_images, test_labels, MODEL_ARCHITECTURE, BATCH_SIZE, EPOCHS, LEARNING_RATE)
 
 runner()
