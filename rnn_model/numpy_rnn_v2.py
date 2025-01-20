a01ad1efb3110457c0fa644445e48099b5391d51
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-20 21:11:10 +0800

    Deep RNN model

--- rnn_model/numpy_rnn_v2.py
+++ rnn_model/numpy_rnn_v2.py
@@ -1,46 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from neurons import recurrent_neurons
 
 # Numpy RNN 🧠
-def NumpyRNNV2(weight_ih, weight_hh, bias_ih, bias_hh, ln_weight, ln_bias):
+def NumpyRNNV2(weight_ih, weight_hh, bias_ih, bias_hh, ln_con_w, ln_con_b, ln_weight, ln_bias):
     # Model stress initializer start with zeros Model is 🏖️🌴
     weight_ih_stress = np.zeros_like(weight_ih)
     bias_ih_stress = np.zeros_like(bias_ih)
-    weight_hh_stress = np.zeros_like(weight_hh)
-    bias_hh_stress = np.zeros_like(bias_hh)
-    ln_weight_stress = np.zeros_like(ln_weight)
-    ln_bias_stress = np.zeros_like(ln_bias)
+    ln_weight_con_stress = np.zeros_like(ln_con_w)
+    ln_bias_con_stress = np.zeros_like(ln_con_b)
+    ln_out_weight_stress = np.zeros_like(ln_weight)
+    ln_out_bias_stress = np.zeros_like(ln_bias)
 
     # Generate_random_params (weight to transport our loss)
-    hidden_to_hidden = np.random.rand(ln_weight.shape[0], weight_hh.shape[0])
+    hidden_to_hidden_mem_1 = np.random.rand(ln_weight.shape[0], weight_hh.shape[0])
+    hidden_to_hidden_mem_2 = np.random.rand(ln_weight.shape[0], weight_hh.shape[0])
 
     # 🧠⏩
     def forward(x, batch_first=True):
         # RNN layer forward pass
-        x = x.unsqueeze(-1).numpy()
-        if batch_first: x = x.transpose(1, 0, 2)
-        seq_len, batch_size, _ = x.shape
-        # model memory start with zeros🤔
-        starting_memory = np.zeros(shape=(batch_size, weight_hh.shape[0]))
-        previous_memory = starting_memory
-        memories = [starting_memory]
-        produce_memories = []
-        for t in range(seq_len):
-            current_memory_state = np.matmul(x[t], weight_ih.T) + bias_ih
-            previous_memory_state = np.matmul(previous_memory, weight_hh.T) + bias_hh
-            activation = np.tanh(current_memory_state + previous_memory_state)
-            produce_memories.append(activation)
-            memories.append(activation)
-            previous_memory = activation
-        rnn_output = np.stack(produce_memories)
-        if batch_first: rnn_output = rnn_output.transpose(1, 0, 2)
+        rnn_output1, memories1 = recurrent_neurons(x, [weight_ih, bias_ih], [weight_hh, bias_hh])
+        # lin_out = np.matmul(rnn_output1, ln_con_w.T) + ln_con_b
+        rnn_output2, memories2 = recurrent_neurons(rnn_output1, [ln_con_w, ln_con_b], [weight_hh, bias_hh])
 
         # output layer
-        output = np.matmul(rnn_output, ln_weight.T) + ln_bias
-        return output, memories
+        output = np.matmul(rnn_output2, ln_weight.T) + ln_bias
+        return output, memories1, memories2
 
-    def backward(model_pred, expected, activations, input_activation):
-        nonlocal weight_ih_stress, bias_ih_stress, weight_hh_stress, bias_hh_stress, ln_weight_stress, ln_bias_stress
+    def backward(model_pred, expected, mem_activations1, mem_activations2, input_activation):
+        nonlocal weight_ih_stress, bias_ih_stress, ln_weight_con_stress, ln_bias_con_stress, ln_out_weight_stress, ln_out_bias_stress
 
         batch, seq_len = expected.shape
         expected = expected.unsqueeze(-1).numpy()
@@ -48,48 +36,53 @@ def NumpyRNNV2(weight_ih, weight_hh, bias_ih, bias_hh, ln_weight, ln_bias):
         neuron_stress = 2*(model_pred - expected)
 
         # Output neurons stress 
-        neurons_memories = np.stack(activations[1:], axis=1).reshape(batch*seq_len, -1)
-        ln_weight_stress += (np.matmul(neuron_stress.reshape(batch*seq_len, -1).transpose(1, 0), neurons_memories) / neurons_memories.shape[0])
-        ln_bias_stress += np.mean(np.mean(neuron_stress, axis=1), axis=0)
-        # Memory neurons stress
-        memories_stress = np.matmul(neuron_stress, hidden_to_hidden)
-        weight_ih_stress += (np.matmul(memories_stress.reshape(batch*seq_len, -1).transpose(1, 0), input_activation.reshape(batch*seq_len, -1)) / (batch*seq_len))
-        bias_ih_stress += np.mean(np.mean(memories_stress, axis=1), axis=0)
-
+        neurons_memories_2 = np.stack(mem_activations2[1:], axis=1).reshape(batch*seq_len, -1)
+        ln_out_weight_stress += (np.matmul(neuron_stress.reshape(batch*seq_len, -1).transpose(1, 0), neurons_memories_2) / neurons_memories_2.shape[0])
+        ln_out_bias_stress += np.mean(np.mean(neuron_stress, axis=1), axis=0)
+        # Memory 2 neurons stress
+        neurons_memories_1 = np.stack(mem_activations1[1:], axis=1).reshape(batch*seq_len, -1)
+        memories_stress_2 = np.matmul(neuron_stress, hidden_to_hidden_mem_2)
+        ln_weight_con_stress += (np.matmul(memories_stress_2.reshape(batch*seq_len, -1).transpose(1, 0), neurons_memories_1) / (batch*seq_len))
+        ln_bias_con_stress += np.mean(np.mean(memories_stress_2, axis=1), axis=0)
+        # Memory 1 neurons stress
+        memories_stress_1 = np.matmul(neuron_stress, hidden_to_hidden_mem_1)
+        weight_ih_stress += (np.matmul(memories_stress_1.reshape(batch*seq_len, -1).transpose(1, 0), input_activation.reshape(batch*seq_len, -1)) / (batch*seq_len))
+        bias_ih_stress += np.mean(np.mean(memories_stress_1, axis=1), axis=0)
         return loss
 
     def update_params():
         # modifiable network stress
-        nonlocal weight_ih_stress, bias_ih_stress, weight_hh_stress, bias_hh_stress, ln_weight_stress, ln_bias_stress
+        nonlocal weight_ih_stress, bias_ih_stress, ln_weight_con_stress, ln_bias_con_stress, ln_out_weight_stress, ln_out_bias_stress
         # modifiable network parameters
-        nonlocal weight_ih, weight_hh, bias_ih, bias_hh, ln_weight, ln_bias
+        nonlocal weight_ih, weight_hh, bias_ih, bias_hh, ln_con_w, ln_con_b, ln_weight, ln_bias
 
         # output layer params
-        ln_weight -= 0.1 * ln_weight_stress
-        ln_bias -= 0.1 * ln_bias_stress
-        # NOTE: We don't update the weights connecting to previous memory and current memory (Can this learn???)
-        # input to hidden params
+        ln_weight -= 0.1 * ln_out_weight_stress
+        ln_bias -= 0.1 * ln_out_bias_stress
+        ln_con_w -= 0.1 * ln_weight_con_stress
+        ln_con_b -= 0.1 * ln_bias_con_stress
         weight_ih -= 0.1 * weight_ih_stress
         bias_ih -= 0.1 * bias_ih_stress
 
         # model back to 🏖️🌴
         weight_ih_stress = np.zeros_like(weight_ih)
         bias_ih_stress = np.zeros_like(bias_ih)
-        weight_hh_stress = np.zeros_like(weight_hh)
-        bias_hh_stress = np.zeros_like(bias_hh)
-        ln_weight_stress = np.zeros_like(ln_weight)
-        ln_bias_stress = np.zeros_like(ln_bias)
+        ln_weight_con_stress = np.zeros_like(ln_con_w)
+        ln_bias_con_stress = np.zeros_like(ln_con_b)
+        ln_out_weight_stress = np.zeros_like(ln_weight)
+        ln_out_bias_stress = np.zeros_like(ln_bias)
 
     def runner(x_train, y_train, x_test, y_test, epochs):
         nonlocal weight_ih, weight_hh, bias_ih, bias_hh, ln_weight, ln_bias
         for epoch in range(epochs):
-            model_pred, model_activations  = forward(x_train)
-            loss = backward(model_pred, y_train, model_activations, x_train.unsqueeze(-1).numpy())
+            input_for_model = x_train.unsqueeze(-1).numpy()
+            model_pred, mem_1, mem_2  = forward(input_for_model)
+            loss = backward(model_pred, y_train, mem_1, mem_2, input_for_model)
             update_params()
             # Check model performance 10 epochs interval
             if (epoch + 1) % 10 == 0: print(f'Epoch [{epoch+1}/{epoch}], Loss: {loss.item():.4f}')
 
-        predictions = forward(x_test)[0].squeeze(2)
+        predictions = forward(x_test.unsqueeze(-1).numpy())[0].squeeze(2)
         # Plot results
         plt.figure(figsize=(10, 6))
         plt.plot(y_test[0], label='True')
