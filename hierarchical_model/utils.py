d9675b9be8a042a057a50fca50b6d6eeee861b38
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-23 10:21:58 +0800

    model and weight transport parameters initializer

--- hierarchical_model/utils.py
+++ hierarchical_model/utils.py
@@ -32,7 +32,40 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=-1, keepdims=True)
 
-def neuron(input_neurons, parameters, readout_parameters):
-    activation = linear_neurons(input_neurons, parameters)
-    neuron_activation = linear_neurons(activation, readout_parameters)
-    return neuron_activation, activation
+def neuron(parameters, shared_parameters):
+
+    def forward_pass(input_neurons):
+        memory = input_neurons
+        memories = []
+        for each in range(len(parameters)):
+            each_params = parameters[each]
+            memory = linear_neurons(memory, each_params)
+            memories.append(memories)
+        neuron_activation = linear_neurons(memory, shared_parameters)
+        return neuron_activation
+
+    return forward_pass
+
+def init_model_parameters(neuron_properties, output_neuron_size):
+    model_parameters = []
+    for _ in range(output_neuron_size):
+        neuron_parameters = []
+        for size in range(len(neuron_properties)-1):
+            input_size = neuron_properties[size]
+            output_size = neuron_properties[size+1]
+            parameters = init_params(input_size, output_size)
+            neuron_parameters.append(parameters)
+        model_parameters.append(neuron_parameters)
+
+    return model_parameters
+
+def init_weights_stress_transport(neuron_properties, output_neurons_size):
+    weights_stress_transport = []
+    for _ in range(output_neurons_size):
+        transport_parameters = []
+        for size in range(len(neuron_properties)-1):
+            weights = np.random.rand(1, neuron_properties[-(size+1)])
+            transport_parameters.append(weights)
+        weights_stress_transport.append(transport_parameters)
+    
+    return weights_stress_transport
